c3188af32cde97b84555bfd352ab6751ff754e14
plasticine9750 <975036719@qq.com>
2023-01-06 18:06:22 +0800

    refactor: remove nav item list and use single item

--- docs/.vitepress/nav.ts
+++ docs/.vitepress/nav.ts
@@ -3,10 +3,5 @@ import { ROUTES } from './routes'
 
 export const nav: DefaultTheme.Config['nav'] = [
   { text: '首页', link: '/' },
-  {
-    text: '分类',
-    items: [
-      { text: 'TypeScript 类型挑战', link: `${ROUTES.TYPE_CHALLENGES}easy` },
-    ],
-  },
+  { text: 'TypeScript 类型挑战', link: `${ROUTES.TYPE_CHALLENGES}easy` },
 ]
