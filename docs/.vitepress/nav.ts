b8fa3c3e9edc3132e9045f695cbe4bc6a30d08cb
plasticine9750 <975036719@qq.com>
2023-01-07 09:36:06 +0800

    fix(type-challenges): fix medium and hard not active nav link

--- docs/.vitepress/nav.ts
+++ docs/.vitepress/nav.ts
@@ -3,5 +3,9 @@ import { ROUTES } from './routes'
 
 export const nav: DefaultTheme.Config['nav'] = [
   { text: '首页', link: '/' },
-  { text: 'TypeScript 类型挑战', link: `${ROUTES.TYPE_CHALLENGES}easy` },
+  {
+    text: 'TypeScript 类型挑战',
+    link: `${ROUTES.TYPE_CHALLENGES}easy`,
+    activeMatch: ROUTES.TYPE_CHALLENGES,
+  },
 ]
