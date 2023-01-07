814345938d6b3b814725cdc904b6bfac400da567
plasticine9750 <975036719@qq.com>
2023-01-07 16:55:24 +0800

    chore(commitlint): add commitlint prompt scope

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -4,7 +4,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   prompt: {
-    scopes: ['type-challenges', 'fe-monitor-system'],
+    scopes: ['type-challenges', 'fe-monitor-system', 'commitlint'],
   },
   rules: {
     'subject-case': [2, 'never', ['upper-case']],
