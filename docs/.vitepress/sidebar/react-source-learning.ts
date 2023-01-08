5a57c8b378106036cd5d6094df9f58692a0a0d26
plasticine9750 <975036719@qq.com>
2023-01-08 10:26:35 +0800

    refactor(react-source-learning): refactor directory strucutre

--- docs/.vitepress/sidebar/react-source-learning.ts
+++ docs/.vitepress/sidebar/react-source-learning.ts
@@ -1,12 +1,26 @@
 import { ROUTES } from '../routes'
 import { DefaultTheme } from 'vitepress'
 
+const REACT_PATH = `${ROUTES.REACT_SOURCE_LEARNING}react`
+const REACT_RECONCILER_PATH = `${ROUTES.REACT_SOURCE_LEARNING}react-reconciler`
+
 export const reactSourceLearning: DefaultTheme.SidebarGroup[] = [
   {
     text: 'React 源码学习',
     items: [
       { text: '介绍', link: `${ROUTES.REACT_SOURCE_LEARNING}introduction` },
-      { text: 'JSX', link: `${ROUTES.REACT_SOURCE_LEARNING}jsx` },
     ],
+  },
+
+  {
+    text: 'react',
+    items: [{ text: 'JSX', link: `${REACT_PATH}/jsx` }],
+    collapsible: true,
+  },
+
+  {
+    text: 'react-reconciler',
+    items: [{ text: 'fiber', link: `${REACT_RECONCILER_PATH}/fiber` }],
+    collapsible: true,
   },
 ]
