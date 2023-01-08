54e87d06523ca9e6a6144874b9466871fded52f3
plasticine9750 <975036719@qq.com>
2023-01-08 16:49:18 +0800

    chore(commitlint): add react-source-learning scope

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -4,7 +4,12 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   prompt: {
-    scopes: ['type-challenges', 'fe-monitor-system', 'commitlint'],
+    scopes: [
+      'type-challenges',
+      'fe-monitor-system',
+      'react-source-learning',
+      'commitlint',
+    ],
   },
   rules: {
     'subject-case': [2, 'never', ['upper-case']],
