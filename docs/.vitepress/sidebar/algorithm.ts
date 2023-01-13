8582af7e7e5decc07e914cbfc5e0a1ed9501a0bc
plasticine9750 <975036719@qq.com>
2023-01-13 13:47:31 +0800

    chore(algorithm): rename sidebar text from `二叉树相关` to `二叉树`

--- docs/.vitepress/sidebar/algorithm.ts
+++ docs/.vitepress/sidebar/algorithm.ts
@@ -5,7 +5,7 @@ const BINARY_TREE = `${ROUTES.ALGORITHM}binary-tree`
 
 export const algorithm: DefaultTheme.SidebarGroup[] = [
   {
-    text: '二叉树相关',
+    text: '二叉树',
     items: [{ text: 'BFS', link: `${BINARY_TREE}/bfs` }],
   },
 ]
