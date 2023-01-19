b331fdb352a91f86da6a26edabfc2c85387b5a62
plasticine9750 <975036719@qq.com>
2023-01-19 23:52:57 +0800

    feat(nest): add basic.md

--- docs/.vitepress/sidebar/backend.ts
+++ docs/.vitepress/sidebar/backend.ts
@@ -6,6 +6,9 @@ const NEST = `${ROUTES.BACKEND}nest`
 export const backend: DefaultTheme.SidebarGroup[] = [
   {
     text: 'NestJS',
-    items: [{ text: '基础使用', link: `${NEST}/basic` }],
+    items: [
+      { text: '基础使用', link: `${NEST}/basic` },
+      { text: '分环境配置', link: `${NEST}/config` },
+    ],
   },
 ]
