8891a9cf22c0a67a5fbb56c3a3c80efabba7fe65
plasticine <975036719@qq.com>
2023-01-22 20:55:06 +0800

    feat(nest): add interceptor.md

--- docs/.vitepress/sidebar/nest.ts
+++ docs/.vitepress/sidebar/nest.ts
@@ -9,6 +9,10 @@ export const nest: DefaultTheme.SidebarGroup[] = [
       { text: '分环境配置', link: `${ROUTES.BACKEND_NEST}config` },
       { text: '异常处理', link: `${ROUTES.BACKEND_NEST}exception` },
       { text: '管道 - 数据转换和验证', link: `${ROUTES.BACKEND_NEST}pipe` },
+      {
+        text: '拦截器 - 统一响应体',
+        link: `${ROUTES.BACKEND_NEST}interceptor`,
+      },
     ],
   },
 ]
