eb035d77ea2cc12829f466b823adbce02f14fe3e
plasticine <975036719@qq.com>
2023-01-27 11:34:58 +0800

    refactor(fe-monitor-system): extract files to specific directories

--- docs/.vitepress/sidebar/fe-monitor-system.ts
+++ docs/.vitepress/sidebar/fe-monitor-system.ts
@@ -1,18 +1,30 @@
 import { ROUTES } from '../routes'
 import { DefaultTheme } from 'vitepress'
 
+const SYSTEM_PROFILE = `${ROUTES.FE_MONITOR_SYSTEM}system-profile`
+const SDK = `${ROUTES.FE_MONITOR_SYSTEM}sdk`
+const SERVER = `${ROUTES.FE_MONITOR_SYSTEM}server`
+
 export const feMonitorSystem: DefaultTheme.SidebarGroup[] = [
   {
     text: '前端监控系统',
     items: [
-      { text: '基础知识', link: `${ROUTES.FE_MONITOR_SYSTEM}basic` },
       {
-        text: '架构设计',
-        link: `${ROUTES.FE_MONITOR_SYSTEM}architecture-design`,
+        text: '系统概况',
+        items: [{ text: '基础知识', link: `${SYSTEM_PROFILE}/basic` }],
       },
+
       {
-        text: '错误监控',
-        link: `${ROUTES.FE_MONITOR_SYSTEM}error-monitor`,
+        text: 'SDK 数据收集',
+        items: [
+          { text: '架构设计', link: `${SDK}/architecture-design` },
+          { text: '错误监控', link: `${SDK}/error-monitor` },
+        ],
+      },
+
+      {
+        text: '服务端建设',
+        items: [{ text: '日志平台', link: `${SERVER}/log-platform` }],
       },
     ],
   },
