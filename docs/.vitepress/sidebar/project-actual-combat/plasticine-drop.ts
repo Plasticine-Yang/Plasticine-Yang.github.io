7d825303cacd620961103494ea0a61e1d15d2908
plasticine9750 <975036719@qq.com>
2023-05-26 14:39:52 +0800

    feat(plasticine-drop): 创建 WebRTC 文章入口

--- docs/.vitepress/sidebar/project-actual-combat/plasticine-drop.ts
+++ docs/.vitepress/sidebar/project-actual-combat/plasticine-drop.ts
@@ -4,6 +4,9 @@ import { ROUTES } from '../../routes'
 export const plasticineDrop: DefaultTheme.SidebarGroup[] = [
   {
     text: '基于 WebRTC 进行数据共享',
-    items: [{ text: '项目介绍', link: `${ROUTES.PROJECT_ACTUAL_COMBAT_PLASTICINE_DROP}introduction/` }],
+    items: [
+      { text: '项目介绍', link: `${ROUTES.PROJECT_ACTUAL_COMBAT_PLASTICINE_DROP}introduction/` },
+      { text: 'WebRTC', link: `${ROUTES.PROJECT_ACTUAL_COMBAT_PLASTICINE_DROP}web-rtc/` },
+    ],
   },
 ]
