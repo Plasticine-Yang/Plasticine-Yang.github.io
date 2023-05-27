8e1f8ead01b5b47a5883d053d4c5fdf6fbf70d99
plasticine <975036719@qq.com>
2023-05-27 14:43:34 +0800

    feat(plasticine-react): reconciler 基础流程

--- docs/.vitepress/sidebar/framework-source-learning/react.ts
+++ docs/.vitepress/sidebar/framework-source-learning/react.ts
@@ -4,6 +4,9 @@ import { DefaultTheme } from 'vitepress'
 export const frameworkSourceLearningReact: DefaultTheme.SidebarGroup[] = [
   {
     text: 'React',
-    items: [{ text: '01 - JSX', link: `${ROUTES.FRAMEWORK_SOURCE_LEARNING_REACT}/01-jsx/` }],
+    items: [
+      { text: '01 - JSX', link: `${ROUTES.FRAMEWORK_SOURCE_LEARNING_REACT}/01-jsx/` },
+      { text: '02 - reconciler 基础架构', link: `${ROUTES.FRAMEWORK_SOURCE_LEARNING_REACT}/02-reconciler-basic/` },
+    ],
   },
 ]
