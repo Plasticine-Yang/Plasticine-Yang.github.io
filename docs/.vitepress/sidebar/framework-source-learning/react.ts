8c444490e0f61c7076af32df6d632dbfebdd7bec
plasticine <975036719@qq.com>
2023-05-28 23:16:47 +0800

    feat(plasticine-react): [WIP] 挂载流程

--- docs/.vitepress/sidebar/framework-source-learning/react.ts
+++ docs/.vitepress/sidebar/framework-source-learning/react.ts
@@ -8,6 +8,7 @@ export const frameworkSourceLearningReact: DefaultTheme.SidebarGroup[] = [
       { text: '01 - JSX', link: `${ROUTES.FRAMEWORK_SOURCE_LEARNING_REACT}/01-jsx/` },
       { text: '02 - reconciler 基础架构', link: `${ROUTES.FRAMEWORK_SOURCE_LEARNING_REACT}/02-reconciler-basic/` },
       { text: '03 - 状态更新机制', link: `${ROUTES.FRAMEWORK_SOURCE_LEARNING_REACT}/03-update/` },
+      { text: '04 - 挂载流程', link: `${ROUTES.FRAMEWORK_SOURCE_LEARNING_REACT}/04-mount/` },
     ],
   },
 ]
